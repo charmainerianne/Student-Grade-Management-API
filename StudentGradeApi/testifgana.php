3b18c98a09cbc786179449074f1fae1df50c90b1
Charmaine Rianne Q. Bordario <158406413+charmainerianne@users.noreply.github.com>
2025-03-09 13:51:55 +0800

    Delete StudentGradeApi directory

--- StudentGradeApi/testifgana.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Charmaine\StudentGradeApi\Controllers\StudentController;
-
-$studentController = new StudentController();
-echo "Class loaded successfully!";
